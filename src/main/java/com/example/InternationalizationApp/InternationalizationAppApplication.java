f2c997f0cddd66cb33fb2a4c2fee20fdaa05bb71
eva <evgeniya.opanasenko@outlook.com>
2018-04-04 15:00:43 +0300

    innit commit

---
+++ src/main/java/com/example/InternationalizationApp/InternationalizationAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.InternationalizationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalizationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternationalizationAppApplication.class, args);
+	}
+}
